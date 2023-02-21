61aa30466f4ace797c6f920f29e97eedaf5ae61d
Abbas Rizvi <abbas.rizvi@sage.com>
2023-02-20 18:15:53 -0800

    added in search bar

--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import SearchBar from "material-ui-search-bar";
 import { Link } from "react-router-dom";
 
 export function Home() {
+  const [movieData, setMovieData] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+
+  useEffect(() => {
+    fetch(
+      "https://api.themoviedb.org/3/movie/popular?api_key=36fa93a60bfe6f4442c5db70e291c96c&language=en-US&page=1"
+    )
+      .then((response) => response.json())
+      .then((data) => setMovieData(data));
+  }, []);
+
+  const onClick = () => {
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=36fa93a60bfe6f4442c5db70e291c96c&language=en-US&query=${searchQuery}&page=1&include_adult=false`;
+    fetch(url)
+      .then((response) => response.json())
+      .then((x) => console.log(x));
+  };
+
   return (
     <>
-      <p>Test Text area</p>
       <nav>
         <ul>
           <li>
@@ -12,6 +30,12 @@ export function Home() {
           </li>
         </ul>
       </nav>
+      <SearchBar
+        onRequestSearch={() => onClick()}
+        placeholder="Search for a movie... e.g. The Dark Knight"
+        onChange={(newValue) => setSearchQuery(newValue)}
+        autoFocus
+      />
     </>
   );
 }
